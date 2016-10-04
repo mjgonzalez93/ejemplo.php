477edb928c5a0e9815c590199e49e34e90ed5d23
Manuel Jesus Gonzalez <m.jesus_1993@hotmail.com>
2016-10-04 14:21:54 +0200

    Mejorada generacion de preguntas en el formulario

--- index.php
+++ index.php
@@ -8,45 +8,24 @@
     <title>Encuesta</title>
 </head>
 <body>
-    <h1>Encuesta de HLC</h1>
-    <form>
+<h1>Encuesta de HLC</h1>
+<form>
+    <?php
+    for ($pregunta = 1; $pregunta <= 5; $pregunta++){
+        echo <<<PREGUNTA
         Pregunta 1
-        <input type="radio" name="pr1" value="1" /> 1
-        <input type="radio" name="pr1" value="2" /> 2
-        <input type="radio" name="pr1" value="3" /> 3
-        <input type="radio" name="pr1" value="4" /> 4
-        <input type="radio" name="pr1" value="5" /> 5
+        <input type="radio" name="pr$pregunta" value="1" /> 1
+        <input type="radio" name="pr$pregunta" value="2" /> 2
+        <input type="radio" name="pr$pregunta" value="3" /> 3
+        <input type="radio" name="pr$pregunta" value="4" /> 4
+        <input type="radio" name="pr$pregunta" value="5" /> 5
         <br/>
-        Pregunta 2
-        <input type="radio" name="pr2" value="1" /> 1
-        <input type="radio" name="pr2" value="2" /> 2
-        <input type="radio" name="pr2" value="3" /> 3
-        <input type="radio" name="pr2" value="4" /> 4
-        <input type="radio" name="pr2" value="5" /> 5
-        <br/>
-        Pregunta 3
-        <input type="radio" name="pr3" value="1" /> 1
-        <input type="radio" name="pr3" value="2" /> 2
-        <input type="radio" name="pr3" value="3" /> 3
-        <input type="radio" name="pr3" value="4" /> 4
-        <input type="radio" name="pr3" value="5" /> 5
-        <br/>
-        Pregunta 4
-        <input type="radio" name="pr4" value="1" /> 1
-        <input type="radio" name="pr4" value="2" /> 2
-        <input type="radio" name="pr4" value="3" /> 3
-        <input type="radio" name="pr4" value="4" /> 4
-        <input type="radio" name="pr4" value="5" /> 5
-        <br/>
-        Pregunta 5
-        <input type="radio" name="pr5" value="1" /> 1
-        <input type="radio" name="pr5" value="2" /> 2
-        <input type="radio" name="pr5" value="3" /> 3
-        <input type="radio" name="pr5" value="4" /> 4
-        <input type="radio" name="pr5" value="5" /> 5
-        <br/>
-        <button type="submit">Votar</button>
-    </form>
+PREGUNTA;
+    }
+?>
+
+    <button type="submit">Votar</button>
+</form>
 </body>
 </html>
 
