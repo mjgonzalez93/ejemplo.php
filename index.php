39fd2903e2d36e80f5e7d0394e3f854a9efa1a09
Manuel Jesus Gonzalez <m.jesus_1993@hotmail.com>
2016-10-04 14:12:20 +0200

    Implementado formulario de la encuesta

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="es" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Encuesta</title>
+</head>
+<body>
+    <h1>Encuesta de HLC</h1>
+    <form>
+        Pregunta 1
+        <input type="radio" name="pr1" value="1" /> 1
+        <input type="radio" name="pr1" value="2" /> 2
+        <input type="radio" name="pr1" value="3" /> 3
+        <input type="radio" name="pr1" value="4" /> 4
+        <input type="radio" name="pr1" value="5" /> 5
+        <br/>
+        Pregunta 2
+        <input type="radio" name="pr2" value="1" /> 1
+        <input type="radio" name="pr2" value="2" /> 2
+        <input type="radio" name="pr2" value="3" /> 3
+        <input type="radio" name="pr2" value="4" /> 4
+        <input type="radio" name="pr2" value="5" /> 5
+        <br/>
+        Pregunta 3
+        <input type="radio" name="pr3" value="1" /> 1
+        <input type="radio" name="pr3" value="2" /> 2
+        <input type="radio" name="pr3" value="3" /> 3
+        <input type="radio" name="pr3" value="4" /> 4
+        <input type="radio" name="pr3" value="5" /> 5
+        <br/>
+        Pregunta 4
+        <input type="radio" name="pr4" value="1" /> 1
+        <input type="radio" name="pr4" value="2" /> 2
+        <input type="radio" name="pr4" value="3" /> 3
+        <input type="radio" name="pr4" value="4" /> 4
+        <input type="radio" name="pr4" value="5" /> 5
+        <br/>
+        Pregunta 5
+        <input type="radio" name="pr5" value="1" /> 1
+        <input type="radio" name="pr5" value="2" /> 2
+        <input type="radio" name="pr5" value="3" /> 3
+        <input type="radio" name="pr5" value="4" /> 4
+        <input type="radio" name="pr5" value="5" /> 5
+        <br/>
+        <button type="submit">Votar</button>
+    </form>
+</body>
+</html>
+
+
